4ab948cd1183e9a050ff3e96352909e0814357a0
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-09-14 10:29:18 +0500

    Create dedicated company details resource

---
+++ app/Http/Resources/CompanyDetailResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyDetailResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug ?? null,
+            'registration_number' => $this->registration_number ?? null,
+            'former_names' => $this->former_names ?? null,
+            'brand_name' => $this->brand_name ?? null,
+            'address' => $this->address ?? null,
+            'phone' => $this->phone ?? null,
+            'email' => $this->email ?? null,
+            'website' => $this->website ?? null,
+            'country' => $this->country ?? null,
+            'country_name' => $this->country_name ?? null,
+            'state_id' => $this->state_id ?? null,
+            'state_name' => $this->state_name ?? null,
+            'created_at' => $this->created_at ?? null,
+            'updated_at' => $this->updated_at ?? null
+        ];
+    }
+}
