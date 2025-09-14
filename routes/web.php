55594b2982026fed088ed6c82075d8ceceb37d92
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-09-14 21:23:21 +0500

    Register new route to get report of a company

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/company/{slug}', [CompanyController::class, 'show'])->name('company
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::get('/cart/company/{companyId}/reports', [CartController::class, 'getCompanyReports'])->name('cart.company.reports');
 Route::delete('/cart/remove/{cartItemId}', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/update/{cartItemId}', [CartController::class, 'update'])->name('cart.update');
 Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
